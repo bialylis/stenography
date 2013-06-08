ed3f4c0f5ea6afd7badd2c3d610b381d5863e019
Matias De Santi <mdesanti90@gmail.com>
2013-06-08 19:10:47 -0300

    i

--- desafio.sh
+++ desafio.sh
@@ -3,13 +3,11 @@ echo "Este script corre lo necesario para generar los archivos de salida a parti
 
 rm -rf ./resources/example2/out/*
 
-echo "1) Extrayendo de ironlady.bmp utilizando LSB4\n\n"
-#//PNG 42852 bytes => .zip => .txt with instructions
-./stegobmp --extract  -p resources/example2/ironlady.bmp -o resources/example2/out/salida2 -s LSB4
+echo "1 -> Extrayendo de ironlady.bmp utilizando LSB4\n\n"
+./stegobmp --extract -p resources/example2/ironlady.bmp -o resources/example2/out/salida2 -s LSB4
 
-echo "2) Extrayendo de hugo.bmp utilizando LSBE\n\n"
-#//PDF 6KB
-./stegobmp --extract  -p resources/example2/hugo.bmp -o resources/example2/out/salida2 -s LSBE
+echo "2 -> Extrayendo de hugo.bmp utilizando LSBE\n\n"
+./stegobmp --extract -p resources/example2/hugo.bmp -o resources/example2/out/salida2 -s LSBE
 
-echo "3) Extrayendo de loimposible.bmp utilizando DES - CBC - pass -> desafio y LSB1\n\n"
-./stegobmp --extract -p resources/example2/loimposible.bmp -o resources/example2/out/video  -s LSB1 -a des -m cbc --pass desafio
+echo "3- > Extrayendo de loimposible.bmp utilizando DES - CBC - pass -> desafio y LSB1\n\n"
+./stegobmp --extract -p resources/example2/loimposible.bmp -o resources/example2/out/video -s LSB1 -a des -m cbc --pass desafio
